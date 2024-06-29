e3d0c5ebaedfde80dd0c3aaf70e83acb83ac01f6
drakenevadie19 <drakennievadje19@gmail.com>
2024-06-29 02:38:17 -0500

    New Change: Adding endpoint for whoami

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// whoami endpoint
+app.get('/api/whoami', (req, res) => {
+  const acceptLanguage = req.headers['accept-language'];
+  const userAgent = req.headers['user-agent'];
+  res.send({ "ipaddress":req.ip, "language": acceptLanguage, "software": userAgent })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
